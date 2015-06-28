4634c5a64bee6565c81361066c1fa639196445c3
Rian Rainey <hello@rian.me>
2015-06-28 07:50:35 -0500

    Can do multiple searches well

--- RoadWeather/RoadWeather/ViewController.swift
+++ RoadWeather/RoadWeather/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController, MKMapViewDelegate, UITextFieldDelegate {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    navigationController?.setNavigationBarHidden(true, animated: false)
+    hideNavigationBar()
     destinationTextField.delegate = self
     startingTextField.delegate = self
     startingTextField.hidden = true
@@ -46,7 +46,7 @@ class ViewController: UIViewController, MKMapViewDelegate, UITextFieldDelegate {
     mapView.showsUserLocation = true
     orientMap(currentLocation!)
     
-    let annotation = MKPointAnnotation()
+    var annotation = MKPointAnnotation()
     annotation.coordinate = currentLocation!.coordinate
     annotation.title = "This is where you are"
     annotation.subtitle = "How did you find me?"
@@ -81,7 +81,7 @@ class ViewController: UIViewController, MKMapViewDelegate, UITextFieldDelegate {
     }
   }
   
-  // MARK: - StartRouteButton
+  // MARK: - View Methods
   func setupStartRouteButton() {
     startRouteButton.layer.cornerRadius = 5
     startRouteButton.layer.borderWidth = 1.0
@@ -97,12 +97,23 @@ class ViewController: UIViewController, MKMapViewDelegate, UITextFieldDelegate {
     startRouteButton.hidden = true
   }
   
+  func hideNavigationBar() {
+    navigationController?.setNavigationBarHidden(true, animated: false)
+  }
+  
+  override func prefersStatusBarHidden() -> Bool {
+    return true
+  }
   // MARK: - IBActions
   @IBAction func unwindToMapView(sender: UIStoryboardSegue) {
     let sourceController: SearchResultsTableViewController = sender.sourceViewController as! SearchResultsTableViewController
     destinationTextField.text = selectedDestination!.name
     
     showRouteButton()
+    hideNavigationBar()
+    getDirections()
+    mapView.removeOverlays(mapView.overlays)
+    mapView.removeAnnotations(mapView.annotations)
   }
   
   @IBAction func searchSubmitted(sender: AnyObject) {
@@ -127,27 +138,49 @@ class ViewController: UIViewController, MKMapViewDelegate, UITextFieldDelegate {
   }
   
   @IBAction func startRoutePressed(sender: AnyObject) {
+    
+  }
+  
+  func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+    if annotation is MKUserLocation {
+      //return nil so map view draws "blue dot" for standard user location
+      return nil
+    }
+    
+    let reuseId = "pin"
+    
+    var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+    if pinView == nil {
+      pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+      pinView!.canShowCallout = true
+      pinView!.animatesDrop = true
+      pinView!.pinColor = .Purple
+    }
+    else {
+      pinView!.annotation = annotation
+    }
+    
+    return pinView
+  }
+  
+  func getDirections() {
     let request = MKDirectionsRequest()
     var current2D = CLLocationCoordinate2DMake(currentLocation!.coordinate.latitude, currentLocation!.coordinate.longitude)
     var currentPlacemark = MKPlacemark(coordinate: current2D, addressDictionary: nil)
     var currentMapItem = MKMapItem(placemark: currentPlacemark)
     request.setSource(currentMapItem)
-//    request.setSource(MKMapItem.mapItemForCurrentLocation())
     request.setDestination(selectedDestination)
     request.requestsAlternateRoutes = false
     
     let directions = MKDirections(request: request)
     
     directions.calculateDirectionsWithCompletionHandler({(response: MKDirectionsResponse!, error: NSError!) in
-      
       if error != nil {
-        // Handle error
         println("There was an error: #{error}")
       } else {
         println("SUCCESS!")
         self.showRoute(response)
       }
-      
     })
   }
   
@@ -161,11 +194,13 @@ class ViewController: UIViewController, MKMapViewDelegate, UITextFieldDelegate {
       }
     }
     
-    var userLocation = mapView.userLocation.location
-    userLocation != nil ? userLocation : currentLocation
-    let region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 2000, 2000)
+    let annotation = MKPointAnnotation()
+    annotation.coordinate = selectedDestination!.placemark.coordinate
+    annotation.title = "This is where you are"
+    annotation.subtitle = "How did you find me?"
+    mapView.addAnnotation(annotation)
     
-    mapView.setRegion(region, animated: true)
+    mapView.showAnnotations(mapView.annotations, animated: true)
   }
   
   func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
